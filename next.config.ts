e59fbe6ea8dcb693c596487802d7d505afaa167e
Asmaa Albermawy <38582333+asmaaalbermawy@users.noreply.github.com>
2025-07-10 21:49:12 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com', // ✅ for QR images
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
